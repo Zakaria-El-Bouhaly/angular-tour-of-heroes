f27ad1d662e4a1cb20958c1761d49523eff2c152
Zakaria El Bouhaly <zelbouhaly@smb.sqli.com>
2024-03-13 08:48:37 +0000

    tutorial

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.scss'
+})
+export class HeroesComponent {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(
+      {
+        next: heroes => this.heroes = heroes
+      });
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
+
+
+}
